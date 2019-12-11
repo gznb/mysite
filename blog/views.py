71e6510bfc4fd75a968f43f031704eb020b7aaa2
gznb <949952519@qq.com>
2019-12-11 17:27:19 +0800

    使用视图类

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-
-# def post_list(request):
-#     # 使用的是我们定义的查询管理器
-#     # 第一版，没有进行分页的的
-#     posts = Post.published.all()
-#     return render(request, 'blog/post/list.html',
-#                   {'posts': posts})
-
-def post_list(request):
-    object_list = Post.published.all()
-    # 相当于有一个默认的 一页 3 个
-    paginator = Paginator(object_list, 3)
-    # 这里有有一个 get 明确指定的
-    page = request.GET.get("page")
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger as err:
-        # 参数错误就显示第一页就好
-        posts = paginator.page(1)
-    except EmptyPage as err:
-        # 如果超过了最大的页数，那就是最后一页
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page, 'posts': posts})
+from django.views.generic import ListView
 
 
 def post_detail(request, year, month, day, post):
@@ -37,4 +11,11 @@ def post_detail(request, year, month, day, post):
                              publish__day = day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
